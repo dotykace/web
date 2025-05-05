e16c8c4ad63ef17b8fa73947846d0fedafab9604
ToChoSK <tchovancak10@gmail.com>
2025-05-05 21:46:31 +0200

    final fiiix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/TouchThePhone',
+  assetPrefix: '/TouchThePhone/',
   eslint: {
     ignoreDuringBuilds: true,
   },
