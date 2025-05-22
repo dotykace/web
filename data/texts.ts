9d67d71e138c20c284cc52d2728e4efcd7cbb1a0
Tomáš Chovančák <tchovancak10@gmail.com>
2025-05-22 11:19:44 +0200

    fix: Code cleanup

--- data/texts.ts
+++
@@ -1,9 +0,0 @@
-export const texts = [
-  "Ahoj, já jsem appka. Jsem kus softwaru, víš jak: 0-1-1-0-0-1... Co ty? Jak ti mám říkat?",
-  "Ok, UN! Řeknu ti, proč mě naprogramovali, jo?",
-  "Jsem tu, abychom spolu hráli hru.",
-  "Ty, UN, jsi v téhle hře prostě ty, jasný?",
-  "Já si hraju na to, že jsem tvůj mobil, který jakoby na chvíli obživnul.",
-  "Dovedeš si to představit? Že chatuješ s vlastním mobilem?",
-  "Většinou ti za sebe píšu jenom: 'battery is low', nakrm mě! Jinak toho moc neřeknu.",
-]
