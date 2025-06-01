9de92ac44e5dbc42a4be66129983ac49d2ac5dc9
DionysusBacchus <xkhrys00@vutbr.cz>
2025-06-01 16:50:48 +0200

    connect chapter 0  to menu

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { useInteractions } from "@/hooks/use-interactions"
 import { ChatProvider } from "@/context/ChatContext"
 import CardSequence from "@/components/CardSequence"
 import Chat from "@/components/Chat"
+import {redirect} from "next/navigation";
 
 const CHAPTER_CONFIG = {
   0: {
@@ -66,6 +67,11 @@ export default function Home() {
       }, 100)
     }
   }, [currentInteraction?.id, setChapter, setFirstInteraction])
+
+  if(!loading && chapter !== 0) {
+    console.log("Redirecting to menu from chapter", chapter)
+    redirect("/menu")
+  }
 
   if (loading) {
     return (
