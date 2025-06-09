571a230ca302aa181de0f78f693065a8450aebe5
DionysusBacchus <xkhrys00@vutbr.cz>
2025-06-09 13:53:51 +0200

    fix branching, add delay, rename end interactions

--- hooks/use-interactions.ts
+++ hooks/use-interactions.ts
@@ -73,12 +73,12 @@ export function useInteractions<T>(filename:string) {
     if (!currentInteraction) return
 
     if (currentInteraction.type === "checkpoint") {
-      if (currentInteraction.id === "chapter-1-begin") {
-        console.log("Chapter 1 start interaction reached, setting chapter to 1")
+      if (currentInteraction.id === "intro-end") {
+        console.log("Introduction end interaction reached, setting chapter to 1")
         setToStorage("chapter", 1)
         redirect("/menu")
-      } else if (currentInteraction.id === "chapter-2-begin") {
-        console.log("Chapter 2 start interaction reached, setting chapter to 2")
+      } else if (currentInteraction.id === "chapter-1-end") {
+        console.log("Chapter 1 end interaction reached, setting chapter to 2")
         setToStorage("chapter", 2)
         redirect("/menu")
       }
@@ -110,7 +110,6 @@ export function useInteractions<T>(filename:string) {
 
   const handleUserInput = useCallback(
       (input: string) => {
-        setUserInput(input)
         // todo id is not really part of currentInteraction, solve it later with further refactoring
         if (currentInteraction?.type === "input") {
           // If this is a username input (id "2")
@@ -128,7 +127,8 @@ export function useInteractions<T>(filename:string) {
           // todo either dont have user input as interaction or make it work properly
         // todo maybe separate user inputs and interactions
         else if (true){
-          if (currentInteraction?.id === "1.6") {
+          if (currentInteraction?.id === "1.6" || currentInteraction?.id === "1.100" || currentInteraction?.id === "1.102") {
+            setToStorage("firstMessage", input)
             goToNextInteraction("1.7")
           }
         }
@@ -160,6 +160,7 @@ export function useInteractions<T>(filename:string) {
 
         const username = readFromStorage("UN") as string
         const botName = readFromStorage("BN") as string
+        const userInput = readFromStorage("firstMessage") as string
 
         let processed = text.replace(/UN/g, username || "ty")
         processed = processed.replace(/BN/g, botName || "Bot")
@@ -167,7 +168,7 @@ export function useInteractions<T>(filename:string) {
 
         return processed
       },
-      [userInput],
+      [],
   )
 
   // todo vsetko musi ist do pice je tu toho moc vela
