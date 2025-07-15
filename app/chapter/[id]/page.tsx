882bc6a2b15ad2209dba7246cd12aa41bc8380a2
ToChoSK <tchovancak10@gmail.com>
2025-07-15 23:43:33 +0200

    fix: Experimental rendering fix

--- app/chapter/[id]/page.tsx
+++ app/chapter/[id]/page.tsx
@@ -2,7 +2,7 @@ import ChapterClient from "./ChapterClient"
 
 export async function generateStaticParams() {
   // Generate static params for all available chapters
-  return [{ id: "0" }, { id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }]
+  return [{ id: "0" }, { id: "1" }]
 }
 
 interface PageProps {
