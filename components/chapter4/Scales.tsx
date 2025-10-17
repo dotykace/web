1435ddd3487da7df603ea4b9eaa429ad2040738c
DionysusBacchus <xkhrys00@vutbr.cz>
2025-10-17 23:06:15 +0200

    store collected scales data

--- components/chapter4/Scales.tsx
+++ components/chapter4/Scales.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ScaleTemplate from "@/components/chapter4/ScaleTemplate";
 
 export default function Scales() {
@@ -17,17 +17,67 @@ export default function Scales() {
       "secondary": "D",
       "top": "klid a bezpečí",
       "bottom": "napětí a úzkost",
-    }
+    },
+    {
+      "type": "scale",
+      "name": "D",
+      "secondary": "E",
+      "top": "sebevědomí",
+      "bottom": "stud",
+      "next-id": "scale-E"
+    },
+    {
+      "type": "scale",
+      "name": "C",
+      "secondary": "A",
+      "top": "vděk",
+      "bottom": "rozhořčení",
+      "next-id": "scale-D"
+    },
+    {
+      "type": "scale",
+      "name": "E",
+      "secondary": "C",
+      "top": "zábava a inspirace",
+      "bottom": "prázdnota",
+      "next-id": "gallery"
+    },
   ]
 
+  const [data, setData] = useState({});
+  const [dataCollected, setDataCollected] = useState(false);
+
+  useEffect(() => {
+    if(dataCollected){
+      console.log("Final data:", data);
+    }
+  }, [data]);
+
+
+  const updateData = (key, value) => {
+    setData(prev => ({
+      ...prev,
+      [key]: value, // dynamically update key
+    }));
+  };
+
   const updateScale = (number) => {
-    console.log(number)
+    updateData(scales[currentScaleIndex].name, number);
     setCurrentScaleIndex((currentScaleIndex + 1) % scales.length);
+    if (isLastScale){
+      setDataCollected(true);
+    }
   }
 
   const currentScale = scales[currentScaleIndex];
+  const isLastScale = currentScaleIndex === scales.length - 1;
 
-  return <ScaleTemplate topText={currentScale.top} bottomText={currentScale.bottom} onConfirm={updateScale} />
-
-
+  return (
+    <ScaleTemplate
+      topText={currentScale.top}
+      bottomText={currentScale.bottom}
+      onConfirm={updateScale}
+      confirmationText={isLastScale ? "Potvrdit a dokončit" : undefined}
+    />
+  )
 }
