363aa928264bd7503cc9d559f8f613698d707bcd
DionysusBacchus <xkhrys00@vutbr.cz>
2025-10-28 21:37:04 +0100

    add optional loaded flag for audio wrapper

--- components/audio/AudioWrapper.tsx
+++ components/audio/AudioWrapper.tsx
@@ -1,19 +1,22 @@
 import {AudioProvider, useSharedAudio} from "@/context/AudioContext";
 import {useEffect} from "react";
 
-function AudioInitializer({soundMap}) {
+function AudioInitializer({soundMap, setLoaded}) {
   const { preloadAll } = useSharedAudio();
 
   useEffect(() => {
-    preloadAll(soundMap).then(() => console.log("Sounds loaded"));
+    preloadAll(soundMap).then(() => {
+      console.log("Sounds loaded")
+      setLoaded && setLoaded(true);
+    });
   }, [preloadAll]);
 
   return null;
 }
-export default function AudioWrapper({ children, soundMap }) {
+export default function AudioWrapper({ children, soundMap, setLoaded }) {
   return (
     <AudioProvider>
-      <AudioInitializer soundMap={soundMap}/>
+      <AudioInitializer soundMap={soundMap} setLoaded={setLoaded}/>
       {children}
     </AudioProvider>
   );
