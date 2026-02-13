3608b443fb472ebae103164bfcd02241c02902b7
DionysusBacchus <xkhrys00@vutbr.cz>
2026-02-13 15:30:32 +0100

    add mute button for full screen video

--- components/FullScreenVideo.tsx
+++ components/FullScreenVideo.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, {useRef} from "react"
+import AudioControl from "@/components/AudioControl";
 
 export default function FullScreenVideo({ videoSrc, onEnded }) {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isMuted, setIsMuted] = React.useState(false)
+
+  const toggleMute = () => {
+    setIsMuted((prev) => !prev)
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted
+    }
+  }
   return (
     <div className="relative w-screen h-screen aspect-video rounded-lg overflow-hidden shadow-lg">
+      <AudioControl onClick={toggleMute} audioEnabled={!isMuted}/>)
       <video
         onEnded={onEnded}
+        ref={videoRef}
         className="absolute top-0 left-0 w-full h-full"
         src={`/videos/${videoSrc}`}
         playsInline
