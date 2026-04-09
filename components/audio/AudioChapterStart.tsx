625937fdf7bfd35a6560c077a84f6488e4af9541
DionysusBacchus <xkhrys00@vutbr.cz>
2026-04-09 17:05:20 +0200

    remove reset option from start screen

--- components/audio/AudioChapterStart.tsx
+++ components/audio/AudioChapterStart.tsx
@@ -1,7 +1,4 @@
-import {useCallback, useState} from "react";
-import {readFromStorage} from "@/scripts/local-storage";
-import {useChatContext} from "@/context/ChatContext";
-import {CHAPTER2_PROGRESS_KEY, CHAPTER3_PROGRESS_KEY} from "@/components/ChapterPage";
+import {useState} from "react";
 
 const ChapterMetaData = {
   2: {
@@ -9,38 +6,21 @@ const ChapterMetaData = {
       text: "text-purple-600",
       button: "bg-purple-600 hover:bg-purple-700 shadow-purple-500/30"
     },
-    progressKey: CHAPTER2_PROGRESS_KEY,
-    startId: "chapter-2-start"
   },
   3: {
     color: {
       text: "text-orange-500",
       button: "bg-orange-500 hover:bg-orange-600 shadow-orange-400/30"
     },
-    progressKey: CHAPTER3_PROGRESS_KEY,
-    startId: "chapter-3-start"
   }
 }
 
 export default function AudioChapterStart({number, component}) {
   const color = ChapterMetaData[number].color
-  const progressKey = ChapterMetaData[number].progressKey
-  const startId = ChapterMetaData[number].startId
   const [hasStarted, setHasStarted] = useState(false)
 
-  const storedProgress = readFromStorage(progressKey)
-  const subHeaderText = storedProgress ? "Máme uložený postup z předchozího hraní. Chceš začít znovu, nebo pokračovat?" : "Jsi ready?"
-  const startButtonText = storedProgress ? "Pokračovat" : "Spustit"
-
-  const {goToNextInteraction} = useChatContext()
-
-  const startChapter = useCallback((resetProgress) => {
-    if (resetProgress) {
-      localStorage.removeItem(progressKey)
-      goToNextInteraction(startId)
-    }
-    setHasStarted(true)
-  }, [goToNextInteraction])
+  const subHeaderText = "Jsi ready?"
+  const startButtonText = "Spustit"
 
   if (!hasStarted) {
     return (
@@ -62,18 +42,8 @@ export default function AudioChapterStart({number, component}) {
             <p className={`mb-8 font-medium text-sm ${color.text}`}>
               {subHeaderText}
             </p>
-            {storedProgress && (
-              <button
-                onClick={() => startChapter(true)}
-                className="w-full mb-4 bg-gray-300 hover:bg-gray-400
-                           text-gray-800 font-bold py-2 px-2 rounded-full shadow-lg shadow-gray-400/30
-                           transition-all duration-300 hover:scale-[1.02] active:scale-[0.98]"
-              >
-                Začít znovu
-              </button>
-            )}
             <button
-              onClick={() => startChapter(false)}
+              onClick={() => setHasStarted(true)}
               className={`w-full ${color.button}
                          text-white font-bold py-2 px-2 rounded-full shadow-lg
                          transition-all duration-300 hover:scale-[1.02] active:scale-[0.98]`}
